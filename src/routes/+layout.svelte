fb675d8561ba5ff984962f915c8ffaca07e40549
Hactuss <157395485+hactuss@users.noreply.github.com>
2026-05-09 19:42:05 +0200

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import favicon from "$lib/assets/favicon.svg";
+    import "../app.scss";
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <link rel="icon" href={favicon} />
+    <title>a_risk</title>
+</svelte:head>
+<nav>
+    <img src="" alt="some" />
+    <a href="">link</a>
+</nav>
+{@render children()}
+<footer>
+    <div></div>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<main>
+    <header>
+        <span class="title_text"
+            ><h1>A</h1>
+            <h1>s</h1>
+            <h1>terisk</h1></span
+        >
+        <h4>by hactus</h4>
+    </header>
+
+    <div class="showcase">
+        <div class="showcase_pannel">
+            <h2>Promotional designs</h2>
+            <h6>Self introduction poster</h6>
+        </div>
+        <div class="showcase_pannel">
+            <h2>Customer desgins</h2>
+            <h6>Promotional poster for the "Lost Terminal" podcast</h6>
+        </div>
+    </div>
+</main>
+
+<style>
+    .title_text {
+        display: flex;
+    }
+    .showcase {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+    .showcase_pannel {
+        width: 100%;
+        margin: 1rem;
+        text-align: center;
+        background-color: rgba(0, 0, 0, 0.3);
+    }
+</style>
